e40779650798779f5ac702c0495703b4cc684c1b
sashatr <sashanowoe1@gmail.com>
2018-06-25 15:02:44 +0300

    tasks

--- start.hs
+++ start.hs
@@ -1,7 +1,30 @@
 module Demo where
 
+-- task 1
 mono :: Char -> Char
 mono x = x
 
 semiMono :: Char -> a -> Char
 semiMono x y = x
+
+
+-- task 2
+sum' :: Integer -> Integer
+sum' x = x ^ 2
+
+apply2 f x = f (f x)
+
+flip'' f y x = f x y
+
+-- \x -> 2 * x + 7 lambda f
+lenVec = \x y -> sqrt $ x^2 + y^2
+
+
+-- task 3
+p1 = ((1, 2), (3, 4))
+p2 = ((3, 4), (5, 6))
+
+sumFstFst = (+) `on` helper
+  where helper pp = fst $ fst pp
+
+sumFstFst' = (+) `on` (\pp -> fst $ fst pp)
