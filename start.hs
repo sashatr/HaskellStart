7a2e69e9f7fb695f16b0993e8acd618ffbfe351d
sasha.tr <you@example.com>
2018-06-25 12:48:50 +0300

    polymorphic functions

--- start.hs
+++ start.hs
@@ -1,22 +1,7 @@
-module Start where
+module Demo where
 
-import Data.Char
+mono :: Char -> Char
+mono x = x
 
--- task #1
-lenVec3 x y z = sqrt( x ^ 2 + y ^ 2 + z ^ 2   )
-
--- task #2
-my_if x = if x > 0 then 1 else (-1)
-
--- task #3
-sign x = if x > 0 then 1 else if x == 0 then 0 else (-1)
-
--- task #4
-infixl 6 *+*
-(*+*) a b = a ^ 2 + b ^ 2
-
-infixl 6 |-|
-(|-|) a b = abs( a - b)
-
--- task #5
-factorial n = if n == 0 then 1 else n * factorial  (n - 1)
+semiMono :: Char -> a -> Char
+semiMono x y = x
