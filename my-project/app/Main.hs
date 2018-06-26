c8d1e8031d052888b739852f49bb4bfd7277e595
sashatr <sashanowoe1@gmail.com>
2018-06-26 15:31:03 +0300

    let-in

--- my-project/app/Main.hs
+++ my-project/app/Main.hs
@@ -17,6 +17,18 @@ analyzeGold 750 = "Great! 750 standard."
 analyzeGold 585 = "Not bad! 585 standard."
 analyzeGold _   = "I don't know such a standard..."
 
+
+calculateTime :: Int -> Int
+calculateTime timeInS =
+  let threshold  = 40
+      correction = 120
+      delta      = 8
+  in
+  if | timeInS <  threshold -> timeInS + correction
+     | timeInS >= threshold -> timeInS + delta + correction
+
+
 main :: IO ()
 -- main = putStrLn $ checkLocalhost "173.194.22.100"
-main = putStrLn (analyzeGold 999)
+-- main = putStrLn (analyzeGold 999)
+calculateTime 100
