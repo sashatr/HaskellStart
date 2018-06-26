24d46e3c0307c8c49f64eb623b90e96b55d61f43
sashatr <sashanowoe1@gmail.com>
2018-06-26 15:07:22 +0300

    gold analyze

--- my-project/app/Main.hs
+++ my-project/app/Main.hs
@@ -8,5 +8,16 @@ checkLocalhost ip =
     then "It's a localhost!"
     else "No, it's not a localhost."
 
+analyzeGold :: Int -> String
+analyzeGold standard =
+  if standard == 999
+    then "Wow! 999 standard!"
+    else if standard == 750
+           then "Great! 750 standard."
+           else if standard == 585
+                  then "Not bad! 585 standard."
+                  else "I don't know such a standard..."
+
 main :: IO ()
-main = putStrLn (checkLocalhost "173.194.22.100")
+-- main = putStrLn $ checkLocalhost "173.194.22.100"
+main = putStrLn (analyzeGold 999)
